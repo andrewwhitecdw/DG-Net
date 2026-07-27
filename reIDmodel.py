d7bec15607cf8af694f8089d21e66c9daac45422
Andrew White <andrewh@cdw.com>
2026-07-27 07:25:01 -0500

    fix: ft_net_middle fails on batch size 1 due to torch.squeeze

--- reIDmodel.py
+++ reIDmodel.py
@@ -193,7 +193,7 @@ class ft_net_middle(nn.Module):
         # x1  n*2048*1*1
         x1 = self.model.avgpool(x)
         x = torch.cat((x0,x1),1)
-        x = torch.squeeze(x)
+        x = x.view(x.size(0), -1)
         x = self.classifier(x)
         return x
 
