da79a21e5008d7e6f6a638c7aa1298ccdd155231
Andrew White <andrewh@cdw.com>
2026-07-27 20:45:55 -0500

    fix: RandomErasing seeds global RNG

--- random_erasing.py
+++ random_erasing.py
@@ -28,25 +28,25 @@ class RandomErasing(object):
         self.sl = sl
         self.sh = sh
         self.r1 = r1
-        random.seed(7)
+        self.rng = random.Random(7)
 
     def __call__(self, img):
 
-        if random.uniform(0, 1) > self.probability:
+        if self.rng.uniform(0, 1) > self.probability:
             return img
 
         for attempt in range(100):
             area = img.size()[1] * img.size()[2]
        
-            target_area = random.uniform(self.sl, self.sh) * area
-            aspect_ratio = random.uniform(self.r1, 1/self.r1)
+            target_area = self.rng.uniform(self.sl, self.sh) * area
+            aspect_ratio = self.rng.uniform(self.r1, 1/self.r1)
 
             h = int(round(math.sqrt(target_area * aspect_ratio)))
             w = int(round(math.sqrt(target_area / aspect_ratio)))
 
             if w < img.size()[2] and h < img.size()[1]:
-                x1 = random.randint(0, img.size()[1] - h)
-                y1 = random.randint(0, img.size()[2] - w)
+                x1 = self.rng.randint(0, img.size()[1] - h)
+                y1 = self.rng.randint(0, img.size()[2] - w)
                 if img.size()[0] == 3:
                     img[0, x1:x1+h, y1:y1+w] = self.mean[0]
                     img[1, x1:x1+h, y1:y1+w] = self.mean[1]
