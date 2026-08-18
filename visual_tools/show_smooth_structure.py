13119f885af8fb92339f9db1573035fccbd503ab
andrewwhitecdw <andrewwhitecdw@users.noreply.github.com>
2026-08-17 19:18:30 -0500

    fix(show-smooth-structure): image and gif saved to cwd (+1 more)
    Signed-off-by: Andrew White <andrewwhitecdw@users.noreply.github.com>
    Signed-off-by: andrewwhitecdw <andrewwhitecdw@users.noreply.github.com>

--- visual_tools/show_smooth_structure.py
+++ visual_tools/show_smooth_structure.py
@@ -127,7 +127,7 @@ with torch.no_grad():
 # save long image
 pic = np.concatenate( (im[0], im[1]) , axis=0)
 pic = Image.fromarray(pic.astype('uint8'))
-pic.save('smooth-s.jpg')
+pic.save(os.path.join(opts.output_folder, 'smooth-s.jpg'))
 
 # save gif
-imageio.mimsave('./smooth-s.gif', gif)
+imageio.mimsave(os.path.join(opts.output_folder, 'smooth-s.gif'), [frame.astype(np.uint8) for frame in gif])
